0b285bea1f5c6eace38d8286dafa3f17ac73560e
thibaut1304 <thibaut.husser@free.fr>
2024-08-01 15:58:19 +0200

    Add encrypted secrets

---
+++ secret.hpp
@@ -0,0 +1,12 @@
+# pragma once
+
+# define WIFI_SSID "ERROR-404"
+# define WIFI_PASSWORD "7bsnJ56xW5FSjK2tka"
+
+# define WWW_USERNAME "esp32"
+# define WWW_PASS "esp32maison13"
+
+# define MQTT_SERVER "192.168.1.93"
+# define MQTT_PORT 1884
+# define MQTT_USERNAME "thib"
+# define MQTT_PASSWORD "maison13"
